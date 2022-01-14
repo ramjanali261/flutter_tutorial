eb8997536cf2e9967bcbc970a9f33be31330331f
RAMJAN <ramjanali261@gmail.com>
2022-01-15 02:41:27 +0530

    day3 completed

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Center(
+          child: Text(
+        "Login Page",
+        style: TextStyle(
+            fontSize: 30, color: Colors.red, fontWeight: FontWeight.bold),
+      )),
+    );
+  }
+}
---
+++ lib/pages/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => LoginPage()
+      },
+    );
+  }
+}
